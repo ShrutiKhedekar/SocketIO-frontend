4175bf9cb17de2970b678c926b3f4127b6bdb88a
Shruti Khedekar <rh0144@accionlabs.in>
2019-10-11 15:45:32 +0530

    disaplyed emails on UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ChatService } from './chat.service';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -10,9 +12,12 @@ export class AppComponent {
   title = 'app';
   message: string;
   messages = [];
+  data = [];
+  emailInfo = [];
 
   constructor(
-    private chatService: ChatService
+    private chatService: ChatService,
+    private http: Http,
   ) { }
 
   sendMessage() {
@@ -22,11 +27,54 @@ export class AppComponent {
   }
 
   ngOnInit() {
-    this.chatService
-      .getMessages()
-      .subscribe((message) => {
-        console.log(message)
-        this.messages.push(message);
-      });
+    //For socket chat
+    // this.chatService
+    //   .getMessages()
+    //   .subscribe((message) => {
+    //     console.log(message)
+    //     this.messages.push(message);
+    //   });
+
+    this.http.get("http://localhost:5000/emails/emails").map((res) => res.json()).subscribe(p => {
+
+
+      // this.data = p.data;
+
+      this.emailInfo = [];
+
+
+
+      p.data.map(x => {
+         console.log(x)
+
+         let obj = {};
+
+         //email body
+        //  let text;
+        // if (x['payload'] && x['payload']['parts'].length != 0) {
+        //   let info = x['payload']['parts'][0]['parts'][0].body.data;
+        //   let buff = new Buffer(info, 'base64');
+        //    text = buff.toString();
+        // }
+
+        // obj['text'] = text;
+        obj['snippet'] = x['snippet'].substring(0, 28) + "...";
+        obj['dateTime'] = new Date(parseInt(x.internalDate));
+
+
+        let from = x['payload']['headers'].filter((i)  => i.name == 'From' );
+        let sub = x['payload']['headers'].filter((i)  => i.name == 'Subject');
+
+        obj['subject'] = sub[0].value;
+        obj['from'] = from[0].value;
+
+        this.emailInfo = [...this.emailInfo, obj];
+        
+      })
+
+
+      console.log(this.emailInfo)
+    })
+
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
@@ -13,7 +14,8 @@ import { ChatService } from './chat.service';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
   providers: [ChatService],
   bootstrap: [AppComponent]
